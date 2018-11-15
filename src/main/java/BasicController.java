25afc5dc7d2ec09e4a92551c7c5492fb37476a11
111 <111>
2018-11-15 14:46:10 +0800

    左边提交

--- src/main/java/BasicController.java
+++ src/main/java/BasicController.java
@@ -2,6 +2,6 @@ public class BasicController {
     public static void main(String[] args) {
         System.out.println("left");
         System.out.println("right");
-
+        System.out.println("bbbbb");
     }
 }
